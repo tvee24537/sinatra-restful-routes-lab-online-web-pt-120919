61b04bd3373a9f3bb155ec0d047eb5bf3ccaec77
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-03-29 22:32:55 +0000

    Done.

---
+++ db/migrate/20200329221604_create_recipe.rb
@@ -0,0 +1,10 @@
+class CreateRecipe < ActiveRecord::Migration
+  def change
+    create_table :recipes do |t|
+      t.string :name
+      t.string :ingredients
+      t.decimal :cook_time
+    end
+  end
+
+end
